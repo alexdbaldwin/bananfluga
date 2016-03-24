e4ff5167964fc03e2a8dab56b6b3796a5e293039
Alex Baldwin <alexander.baldwin@mah.se>
2016-03-24 23:08:37 +0100

    Added basic AI.

---
+++ bananfluga/Assets/EnemyController.cs
@@ -0,0 +1,145 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class EnemyController : MonoBehaviour {
+
+    public enum AIState {
+        Wander,
+        Attack,
+        Chase
+    }
+
+    Rigidbody rigidBody;
+    CapsuleCollider capsuleCollider;
+    AIState currentState = AIState.Wander;
+    Vector3 targetLocation;
+    float timeUntilChangeTarget = 0.0f;
+    float targetSpeed = 4.0f;
+
+    public float turnSpeed = 1.0f;
+    public float changeTargetInterval = 2.0f;
+    public float mergeRange = 0.05f;
+
+	// Use this for initialization
+	void Start () {
+        rigidBody = GetComponent<Rigidbody>();
+        capsuleCollider = GetComponent<CapsuleCollider>();
+    }
+
+        //if ((Mathf.Abs(input.x) > float.Epsilon || Mathf.Abs(input.y) > float.Epsilon) && (advancedSettings.airControl || m_IsGrounded))
+        //{
+        //    // always move along the camera forward as it is the direction that it being aimed at
+        //    Vector3 desiredMove = cam.transform.forward * input.y + cam.transform.right * input.x;
+        //    desiredMove = Vector3.ProjectOnPlane(desiredMove, m_GroundContactNormal).normalized;
+
+        //    desiredMove.x = desiredMove.x * movementSettings.CurrentTargetSpeed;
+        //    desiredMove.z = desiredMove.z * movementSettings.CurrentTargetSpeed;
+        //    desiredMove.y = desiredMove.y * movementSettings.CurrentTargetSpeed;
+        //    if (m_RigidBody.velocity.sqrMagnitude <
+        //        (movementSettings.CurrentTargetSpeed * movementSettings.CurrentTargetSpeed))
+        //    {
+        //        m_RigidBody.AddForce(desiredMove * SlopeMultiplier(), ForceMode.Impulse);
+        //    }
+        //}
+
+	
+	// Update is called once per frame
+	void FixedUpdate () {
+        switch (currentState)
+        {
+            case AIState.Wander:
+                Wander();
+                break;
+            case AIState.Attack:
+                break;
+            case AIState.Chase:
+                break;
+            default:
+                break;
+        }
+        TransitionState();
+    }
+
+    void Wander() {
+        timeUntilChangeTarget -= Time.deltaTime;
+        if(timeUntilChangeTarget <= 0)
+        {
+            timeUntilChangeTarget += changeTargetInterval;
+            PickRandomWanderTarget();
+        }
+
+        transform.forward = Vector3.Lerp(transform.forward, Vector3.Normalize(targetLocation - transform.position), turnSpeed * Time.deltaTime);
+
+        rigidBody.velocity = transform.forward * targetSpeed;
+
+        GameObject mergeTarget = GetNearestAlly(3f);
+        if(mergeTarget != null)
+        {
+            //Debug.Log("SET PHASERS TO KILL");
+            if (mergeTarget.transform.localScale.magnitude > transform.localScale.magnitude)
+            {
+                mergeTarget.GetComponent<EnemyController>().SizeUp();
+                Destroy(gameObject);
+            }
+            else
+            {
+                Destroy(mergeTarget);
+                SizeUp();
+            }
+        }
+    }
+
+    void PickRandomWanderTarget()
+    {
+        float wanderX = Random.Range(transform.position.x - 10.0f, transform.position.x + 10.0f);
+        float wanderZ = Random.Range(transform.position.z - 10.0f, transform.position.z + 10.0f);
+        targetLocation = new Vector3(wanderX, transform.position.y, wanderZ);
+    }
+
+    void Attack() {
+
+    }
+
+    void SizeUp() {
+        transform.position += new Vector3(0,capsuleCollider.bounds.size.y/2.0f, 0);
+        transform.localScale = transform.localScale * 2.0f;
+        targetLocation.y = transform.position.y;
+
+    }
+
+    void Chase() {
+
+    }
+
+    void TransitionState()
+    {
+        switch (currentState)
+        {
+            case AIState.Wander:
+                
+                break;
+            case AIState.Attack:
+                break;
+            case AIState.Chase:
+                break;
+            default:
+                break;
+        }
+    }
+
+    GameObject GetNearestAlly(float range) {
+        float nearestDist = float.PositiveInfinity;
+        GameObject nearest = null;
+        foreach(GameObject go in GameObject.FindGameObjectsWithTag("Enemy"))
+        {
+            float dist = Vector3.Distance(go.transform.position, transform.position);
+            //Debug.Log(dist);
+            if (dist < nearestDist && dist < range && go != gameObject)
+            {
+                nearestDist = dist;
+                nearest = go;
+            }
+        }
+        return nearest;
+    }
+}
---
+++ bananfluga/Assets/EnemySpawner.cs
@@ -0,0 +1,45 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class EnemySpawner : MonoBehaviour {
+
+    public GameObject enemyType;
+    public BoxCollider spawnRegion;
+    public float spawnInterval = 0.5f;
+
+    public GameObject ground;
+    int minEnemies = 150;
+    float secondsUntilNextSpawn = 0.0f;
+
+
+	// Use this for initialization
+	void Start () {
+        int numEnemies = GameObject.FindGameObjectsWithTag("Enemy").Length;
+        for(int i = numEnemies; i < minEnemies; i++)
+        {
+            SpawnEnemy();
+        }
+	}
+
+    void SpawnEnemy() {
+        Vector3 spawnLocation = new Vector3(Random.Range(spawnRegion.bounds.min.x, spawnRegion.bounds.max.x), ground.GetComponent<MeshCollider>().bounds.max.y  + 1, Random.Range(spawnRegion.bounds.min.z, spawnRegion.bounds.max.z));
+        GameObject.Instantiate(enemyType, spawnLocation, Quaternion.identity);
+    }
+	
+	// Update is called once per frame
+	void Update () {
+        int numEnemies = GameObject.FindGameObjectsWithTag("Enemy").Length;
+        for (int i = numEnemies; i < minEnemies; i++)
+        {
+            SpawnEnemy();
+            //Debug.Log("Spawning!");
+        }
+
+        secondsUntilNextSpawn -= Time.deltaTime;
+        if(secondsUntilNextSpawn <= 0)
+        {
+            SpawnEnemy();
+            secondsUntilNextSpawn += spawnInterval;
+        }
+    }
+}
