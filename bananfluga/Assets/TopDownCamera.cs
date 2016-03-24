c05fb0a53d7828f80bd99f99bf25b0a039acd0ff
Alex Baldwin <alexander.baldwin@mah.se>
2016-03-25 00:40:18 +0100

    Camera script added.

---
+++ bananfluga/Assets/TopDownCamera.cs
@@ -0,0 +1,23 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class TopDownCamera : MonoBehaviour {
+
+    GameObject ball1, ball2;
+    public float cameraSpeed = 2.0f;
+    public GameObject ground;
+    public float heightFactor = 4.0f;
+
+	// Use this for initialization
+	void Start () {
+        ball1 = GameObject.FindGameObjectsWithTag("Player")[0];
+        ball2 = GameObject.FindGameObjectsWithTag("Player")[1];
+    }
+	
+	// Update is called once per frame
+	void Update () {
+        Vector3 ballCenter = (ball1.transform.position + ball2.transform.position)/2.0f;
+        ballCenter.y = ground.GetComponent<MeshCollider>().bounds.max.y + Vector3.Distance(ball1.transform.position, ball2.transform.position) * Mathf.Log(heightFactor);
+        transform.position = Vector3.Lerp(transform.position, ballCenter, Time.deltaTime * cameraSpeed);
+	}
+}
